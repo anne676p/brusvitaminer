c591025174f5b5fe42a75ba1ec34966306ce9019
Anne-Kathrine Jessien <anne-kathrine@AnneKatrinesMBP.domain_not_set.invalid>
2020-10-13 08:30:02 +0200

    javscript virker

--- script.js
+++ script.js
@@ -1,53 +1,67 @@
-gsap.registerPlugin(ScrollTrigger);
-const root = document.documentElement;
+window.addEventListener("load", start);
 
-const sections = [...document.querySelectorAll("section")];
+function start() {
+  gsap.registerPlugin(ScrollTrigger);
+  const root = document.documentElement;
 
-let options = {
-  rootMargin: "0px 0px",
-  threshold: .5
-};
+  const sections = [...document.querySelectorAll("section")];
 
-const callback = (entries, observer) => {
-  entries.forEach(entry => {
-    const {
-      target
-    } = entry;
+  let options = {
+    rootMargin: "0px 0px",
+    threshold: .5
+  };
 
-    if (entry.intersectionRatio >= 0.5) {
-      root.style.setProperty('--hue', target.dataset.hue);
-    }
-  });
-};
+  const callback = (entries, observer) => {
+    entries.forEach(entry => {
+      const {
+        target
+      } = entry;
 
-const observer = new IntersectionObserver(callback, options);
+      if (entry.intersectionRatio >= 0.5) {
+        root.style.setProperty('--hue', target.dataset.hue);
+      }
+    });
+  };
 
-sections.forEach((section, index) => {
-  observer.observe(section)
-})
+  const observer = new IntersectionObserver(callback, options);
 
-sections.forEach((section) => {
-  const divs = section.querySelectorAll("div:not([role='tablist'])");
-
-  gsap.from(divs, {
-    opacity: 0,
-    y: 100,
-    duration: 1,
-    stagger: 1,
-    scrollTrigger: {
-      trigger: section,
-      start: "top center",
-      end: "center center",
-      markers: true,
-      scrub: true,
-    },
-  });
-});
-
-const buttons = document.querySelectorAll('[data-toggle="collapse"]');
-
-buttons.forEach(button => {
-  button.addEventListener('click', _ => {
-    ScrollTrigger.refresh(true);
+  sections.forEach((section, index) => {
+    observer.observe(section)
   })
-});
+
+  sections.forEach((section) => {
+    const divs = section.querySelectorAll("div:not([role='tablist'])");
+
+    gsap.from(divs, {
+      opacity: 0,
+      y: 100,
+      duration: 1,
+      stagger: 1,
+      scrollTrigger: {
+        trigger: section,
+        start: "top center",
+        end: "center center",
+        markers: true,
+        scrub: true,
+      },
+    });
+  });
+
+  const buttons = document.querySelectorAll('[data-toggle="collapse"]');
+
+  buttons.forEach(button => {
+    button.addEventListener('click', _ => {
+      ScrollTrigger.refresh(true);
+    })
+  });
+
+  document.querySelector("#symbol_gron").addEventListener("click", klikGron);
+
+}
+
+function klikGron() {
+  console.log("klikgron");
+
+  document.getElementById("symbol_gron").classList.remove("hide");
+  document.getElementById("symbol_gron").classList.add("gronfill");
+}
